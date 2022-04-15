540e2be69fe56f904d427da549263d060ad00a02
prajktavk2203 <prajktavk2203@gmail.com>
2022-04-16 01:42:20 +0530

    Get JWKS endpoint using Rest Assured

---
+++ src/test/java/restAsscureTests/GetJWKDetails.java
@@ -0,0 +1,29 @@
+package restAsscureTests;
+
+import org.testng.annotations.Test;
+
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matcher.*;
+public class GetJWKDetails 
+{
+	@Test
+	public void getJWKSDetails() 
+	{
+		given()
+		 .when()
+		   .get("https://auth-api.authqapoc.aws.greenwayhealth.com/oauth2/jwks")
+		 .then()
+		    .statusCode(200)
+		    .assertThat().body("alg",equalTo("RS256"))
+		    .header("Content-Type","application/json; charset=utf-8");
+	}
+
+	private ResponseAwareMatcher<Response> equalTo(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
